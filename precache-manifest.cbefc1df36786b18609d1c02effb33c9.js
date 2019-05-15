8286bb236c9335432783404497cfb9186c6fe00a
hourouer <31594279+hourouer@users.noreply.github.com>
2019-05-15 22:42:55 +0800

    test

---
+++ precache-manifest.cbefc1df36786b18609d1c02effb33c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314c2dba52f9d10f730166f2c3b6420d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da147488af0c9b7db4dd",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "da147488af0c9b7db4dd",
+    "url": "/static/js/main.77c2a8b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
